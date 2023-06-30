c388e25e35bc164a022f69e778524ca185c3996f
Daniel Lourenço Affonso <86497631+Daniel-online@users.noreply.github.com>
2023-06-30 20:18:58 -0300

    criando o projeto

---
+++ projetoJava/veiculosJava/src/carro.java
@@ -0,0 +1,9 @@
+
+public class carro extends veiculo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ projetoJava/veiculosJava/src/veiculo.java
@@ -0,0 +1,11 @@
+
+public class veiculo {
+double preco;
+motor motor = new motor();
+ String placa = "000000";
+ 
+ public static void constructor1(String, args){
+	 
+ }
+}
+
